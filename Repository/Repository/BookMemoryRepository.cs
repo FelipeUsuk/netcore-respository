a4cab039bda753b1f055fb65500058b634fb455f
FelipeUsuk <felipeusuk@gmail.com>
2018-02-27 15:33:02 -0300

    corrigindo comentario

--- Repository/Repository/BookMemoryRepository.cs
+++ Repository/Repository/BookMemoryRepository.cs
@@ -7,6 +7,6 @@ namespace Repository.Repository
 {
     public class BookMemoryRepository : MemoryRepositoryBase<BookEntity, int> , IBookRepository<BookEntity, int>
     {
-        //0 code because inherites for abstract repository has all default implementation
+        //0 code because inherites from abstract repository that has all default implementation
     }
 }
